6ea087bd165b1e70e1b9c83ac6bc939f6ca3b90b
LizW <142850386+LizW25@users.noreply.github.com>
2024-02-05 15:24:14 -0800

    setup

--- src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
@@ -1,39 +1,22 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkFlex;
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.RobotContainer;
 
 public class ManipulatorSubsystem extends SubsystemBase {
 
     private final CANSparkFlex intakeMotor;
     private final CANSparkFlex shootMotor;
-    private Boolean intakeMode;
-    private Boolean shootMode;
 
     public ManipulatorSubsystem(RobotContainer container) {
         intakeMotor = new CANSparkFlex(0, MotorType.kBrushless);
         shootMotor = new CANSparkFlex(1, MotorType.kBrushless);
-        intakeMode = false;
-        shootMode = false;
     }
 
-    // can object name it global, create it, method running stoping
-    /*
-     * private void activate() {
-     * active = true;
-     * }
-     */
-
     public void intake() {
-        if (intakeMode = true) {
-            intakeMotor.set(0.3);
-
-        }
 
     }
 
@@ -41,11 +24,7 @@ public class ManipulatorSubsystem extends SubsystemBase {
      * Starts outtaking
      */
     public void shoot() {
-        if (intakeMode = true) {
-            shootMode = true;
-            shootMotor.set(0.3);
-            // activate();
-        }
+
     }
 
     /**
@@ -54,6 +33,5 @@ public class ManipulatorSubsystem extends SubsystemBase {
     public void stoptake() {
         intakeMotor.stopMotor();
         shootMotor.stopMotor();
-
     }
 }
