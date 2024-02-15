efb05045db20a09a504b3e310b4b05365ab19b6f
AriesPowv <143663708+AriesPowv@users.noreply.github.com>
2024-02-14 17:44:48 -0800

    Motor ID Values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String CAN_BUS_NAME_ROBORIO = "rio";
     public static final String CAN_BUS_NAME_DRIVETRAIN = CAN_BUS_NAME_CANIVORE;
 
-    // IO Controller definitions
+    // Contoller IDs
     public static final int CONTROLLER_USB_PORT_DRIVER = 0; // Drivers Controller
     public static final int CONTROLLER_USB_PORT_OPERATOR = 1; // Ordanence operators controller
 
@@ -13,10 +13,15 @@ public class Constants {
 
     // CHASSIS Subsystem
     public static final String TEST_ROBORIO_SERIAL_NUMBER = "0316b2d6"; // serial number of Swervee roborio
-    public static final int LEFT_ARM_MOTOR_ID = 0;
 
     // ARM Subsystem
-    public static final int RIGHT_ARM_MOTOR_ID = 0;
-    public static final int ARM_ENCODER_ID = 0;
+    public static final int RIGHT_ARM_MOTOR_ID = 6;
+    public static final int LEFT_ARM_MOTOR_ID = 7;
+    public static final int ARM_ENCODER_ID = 25;
 
+    // MANIPUlATIR Subsystem
+    public static final int SHOOTER_MOTOR_A_ID = 31;
+    public static final int SHOOTER_MOTOR_B_ID = 32;
+    public static final int INTAKE_MOTOR_ID = 33;
+    public static final int SENSOR_PORT = 0;
 }
--- src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
@@ -3,35 +3,54 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.RobotContainer;
 
 public class ManipulatorSubsystem extends SubsystemBase {
 
+    private final CANSparkFlex shooterMotorA;
+    private final CANSparkFlex shooterMotorB;
     private final CANSparkFlex intakeMotor;
-    private final CANSparkFlex shootMotor;
+    private final DigitalInput noteSensor;
+
+    private boolean previousNoteSensorRead;
+
+    private final double shooterSpeed = 0.8;
+    private final double intakeSpeed = 0.3;
 
     public ManipulatorSubsystem(RobotContainer container) {
-        intakeMotor = new CANSparkFlex(0, MotorType.kBrushless);
-        shootMotor = new CANSparkFlex(1, MotorType.kBrushless);
+        shooterMotorA = new CANSparkFlex(Constants.SHOOTER_MOTOR_A_ID, MotorType.kBrushless);
+        shooterMotorB = new CANSparkFlex(Constants.SHOOTER_MOTOR_B_ID, MotorType.kBrushless);
+        intakeMotor = new CANSparkFlex(Constants.INTAKE_MOTOR_ID, MotorType.kBrushless);
+        noteSensor = new DigitalInput(Constants.SENSOR_PORT);
+        previousNoteSensorRead = noteSensor.get();
     }
 
-    public void intake() {
-
+    public void startShooter() {
+        shooterMotorA.set(shooterSpeed);
+        shooterMotorB.set(shooterSpeed);
     }
 
-    /**
-     * Starts outtaking
-     */
-    public void shoot() {
-
+    public void stopShooter() {
+        shooterMotorA.stopMotor();
+        shooterMotorB.stopMotor();
     }
 
-    /**
-     * Stops taking
-     */
-    public void stoptake() {
+    public void startIntake() {
+        intakeMotor.set(intakeSpeed);
+    }
+
+    public void stopIntake() {
         intakeMotor.stopMotor();
-        shootMotor.stopMotor();
+    }
+
+    public void checkNote() {
+        if (previousNoteSensorRead != noteSensor.get()) {
+            stopIntake();
+        }
+
+        previousNoteSensorRead = noteSensor.get();
     }
 }
