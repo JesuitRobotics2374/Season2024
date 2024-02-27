52a588e4d4f2cb6d31591ceec8d54386af9f0459
JS10123 <apowvalla26@jesuitmail.org>
2024-02-27 15:12:56 -0800

    Revert "Merge branch 'main' of https://github.com/JesuitRobotics2374/Season2024"
    This reverts commit abfe9f7e062dad3d973f0dd5b8518cc9345d0c0e, reversing
    changes made to 517f786a722f7ed1359db2d2115475b99e396f1b.

--- src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
@@ -9,7 +9,7 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants; 
+import frc.robot.Constants;
 
 public class ManipulatorSubsystem extends SubsystemBase {
 
@@ -41,23 +41,6 @@ public class ManipulatorSubsystem extends SubsystemBase {
             intake = true;
         }
         startIntake();
-    }
-
-    public void shoot(double distance) {
-        // double angle = calculateAngle(Constants.AIR_DRAG, Constants.DELTA_HEIGHT, shooterSpeedToMPerS(), distance, )
-    }
-
-    public void shooterSpeedToMPerS() {
-        return ;
-    }
-
-    public double calculateAngle(double a, double h, double v, double d, double n, double sqrtVal, double sqrtVal2, double tanVal) {
-        if (d != (0.451601 * a * v + 0.451601 * h * v) && v * (d * (sqrtVal - 15795133 * h) + h * v * (7133094 * a + 7133094 * h) + 7133094 * d * d * v) != 0) {
-            double atanVal = Math.atan((Math.sqrt(v * v * (50881030012836l * h * h - 50881030012836l * a * a) + 225336336863004l * a * d * v + d * d * (50881030012836l * v * v - 249486226487689l)) + 7133094 * d * v) / (-7133094 * a * v + 15795133 * d - 7133094 * h * v));
-            return 2 * (atanVal + 3.14159 * n);
-        } else {
-            return Double.NaN;
-        }
     }
 
     public void startShooter() {
