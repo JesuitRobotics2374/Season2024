f28d9cffff0a11e6159eb4bcce2077e960574a9b
Aries Powvalla <74034072+AriesNinjaDev@users.noreply.github.com>
2024-03-12 16:52:11 -0700

    finish

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -26,6 +26,7 @@ public class ArmSubsystem extends SubsystemBase {
     ShuffleboardTab tab = Shuffleboard.getTab(Constants.DRIVER_READOUT_TAB_NAME);
     double goal;
     double speed;
+    double manualOffset = 0;
 
     static ArmSubsystem instance;
 
@@ -138,7 +139,7 @@ public class ArmSubsystem extends SubsystemBase {
             }
         }
 
-        return Math.toDegrees((Math.PI / 2) - ((top + bottom) / 2));
+        return Math.toDegrees((Math.PI / 2) - ((top + bottom) / 2)) + manualOffset;
     }
 
     public void shoot() {
@@ -146,6 +147,16 @@ public class ArmSubsystem extends SubsystemBase {
         double angle = myAngleCalculator(CommandSwerveDrivetrain.getInstance().getDistanceToSpeaker());
         setGoal(angle);
         // System.out.println(angle / 360);
+    }
+
+    public void increaseOffset() {
+        manualOffset += 0.5;
+        System.out.println(manualOffset);
+    }
+
+    public void decreaseOffset() {
+        manualOffset -= 0.5;
+        System.out.println(manualOffset);
     }
 
     public static ArmSubsystem getInstance() {
